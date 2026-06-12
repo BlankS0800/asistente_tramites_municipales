0cdae10a005e07bddffb2743011a8db0ba82e686
BlankS0800 <perezovandohernan@gmail.com>
2026-06-12 16:21:22 -0400

    cambio en vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/satic-frontend-gamlp/',
+  base: '/asistente-tramites-municipales/',
   server: {
     proxy: {
       '/api': {
