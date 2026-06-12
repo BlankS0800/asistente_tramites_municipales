fc5af1e09eb8f7e99a0c508ead687ab568ab3faf
BlankS0800 <perezovandohernan@gmail.com>
2026-06-12 16:33:47 -0400

    github agregado

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/asistente-tramites-municipales/',
+  base: '/asistente_tramites_municipales/',
   server: {
     proxy: {
       '/api': {
